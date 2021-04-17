2ca03fa67371caaf630e01cf5936f4b42790c5f5
sharlq <shehabakhader@gmail.com>
2021-04-17 14:33:22 +0300

    there is still a bug with quicksort

--- src/components/sort.jsx
+++ src/components/sort.jsx
@@ -1,68 +1,93 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import sort from "./sorting"
 const Sort = () => {
-const [array,setArray] = useState([2,5,3,8,7,1,10,4,6,9])
+const [array,setArray] = useState([])
 const PRIMARY_COLOR = "rgb(0, 132, 255)";
 const SECONDARY_COLOR ="rgb(221, 224, 0)";
 const PIVOT_COLOR = "green";
+const SORTING_SPEED = 5;
+const NUMBER_OF_ARRAY_BARS = 50;
+function randomIntFromInterval(min,max)
+{
+    return Math.floor( Math.random()*  ( max - min + 1 ) + min );
+}
+
+const resetArray =()=> {
+    const arr = [];
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+      arr.push(randomIntFromInterval(5, 730));
+    }
+    setArray(arr);
+  }
+useEffect(()=>resetArray(),[])
 
 
-
-
- let m = [2,5,3,8,7,1,10,4,6,9];
 const animate = ()=>{
-let aouw = [...array]
-let	animations = sort(aouw,0,array.length ,[])
+
+let	animations = sort(array,0,array.length ,[])
 let i = 0;
-//console.log(animations)
-	const myloop=()=>{
+let pivotTemp ;
+	const myloop= ()=>{
+		
 		const arrBar =document.getElementsByClassName("block")
 		const isColor =i%4==1||i%4==2;
 		const isSwitch=i%4==3;
-		if(isColor){
+		const isPivot =i%4===0;
+		if(isPivot){
+			
+			
+			// let pivotIdx = animations[i];
+			// let pivot = arrBar[pivotIdx].style;
+			// if(animations[i]>animations.length-1){pivotIdx=animations.length-1}
+			// console.log(animations[i],i)
+			// setTimeout(()=>{
+			// console.log(pivotIdx)
+			// pivot.backgroundColor = PIVOT_COLOR;  
+			// pivotTemp = pivot;
+			// },i*SORTING_SPEED)
+			
+			
+		}else if(isColor){
+
 			let [barOneIdx,barTwoIdx] = animations[i];
-			if(barOneIdx>9){barOneIdx=9}
+			if(barOneIdx>animations.length-1){barOneIdx=animations.length-1}
 			const barOneStyle = arrBar[barOneIdx].style;
-			if(barTwoIdx>9){barTwoIdx=9}
+			if(barTwoIdx>animations.length-1){barTwoIdx=animations.length-1}
 			const barTwoStyle = arrBar[barTwoIdx].style;
 			const color =	i%4 === 2 ? PRIMARY_COLOR : SECONDARY_COLOR;
-			 setTimeout(()=>{
+			setTimeout(()=>{
 				barOneStyle.backgroundColor = color;
          		barTwoStyle.backgroundColor = color;
-			},i*500) //note the - i - that means that i dont understand setTimeout
+			},i*SORTING_SPEED) //note the - i - that means that i dont understand setTimeout
+		
 		}else if(isSwitch){
 
 		
-			console.log("switch",animations[i],i,animations.length)
+			
+			// this function is not the right one for change
 			let temp = animations[i];
 			setTimeout(() => {
-			
-				
-				
 				const [barOneIdx, barTwoIdx] = temp; // the problem here was that when i put animation[i] its stuck on one value
 				const barOneStyle = arrBar[barTwoIdx].style;
-				barOneStyle.height = `${barTwoIdx*10}px`;
-					console.log(barOneIdx,"fk")
-
-		
-				
-
-			  }, i * 500);}//it look like it set the hight but it is not changing its the same
-			  // wait it only set the last bar
+				const barTwoStyle = arrBar[barOneIdx].style;
+				barOneStyle.height = `${array[barTwoIdx]}px`;
+				barTwoStyle.height = `${array[barOneIdx]}px`;
+				// pivotTemp.backgroundColor = PRIMARY_COLOR;	
+			  }, i * SORTING_SPEED);}
+			    
 		i++
-		if(i<animations.length-3){
+		if(i<animations.length-1){
 		myloop()}
 			
 	}
 myloop()
 	}
- // there is problem here3
- // untile now i have failes changing the positions of the divs them selfs63
+ 
     return (
         <div className="array">
             {array.map( 
-                (i)=><div key={i} id={i} className="block" style={{height:`${i*10}px`}}></div>)
+                (value,index)=><div key={index} id={index} className="block" style={{height:`${value}px`}}></div>)
                 
                 }
                 <button onClick={()=>animate()}>sort</button>
--- src/components/sorting.js
+++ src/components/sorting.js
@@ -2,13 +2,15 @@
 let animation =[]
 function  quickSortR(arr, left, right)
 {
+	let IS_SWITCHED = 0;
 	var i = left;
 	var j = right;
 	var tmp;
 	var pivotidx = (left + right) / 2; 
 	var pivot = parseInt(arr[pivotidx.toFixed()]);
+	
 	if(pivot){  
-	animation.push(pivot)}
+	animation.push(pivotidx)}
 	else{
 		animation.push(i)
 	}
@@ -16,24 +18,29 @@ function  quickSortR(arr, left, right)
 
 	while (i <= j)
 	{
+		
 		while (parseInt(arr[i]) < pivot)i++;
 		while (parseInt(arr[j]) > pivot)j--;
-		if(j>9){j=9}
+		if(j>arr.length-1){j=arr.length-1}
 		animation.push([i,j])
 		animation.push([i,j])
-		if(i>9||j>9){console.log(animation.length)}
-		if (i <= j)
+		if(i>arr.length-1||j>arr.length-1){console.log(animation.length)}
+		if (i <= j )
 		{
 			
 			tmp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = tmp;
+			IS_SWITCHED = 1;
+			animation.push([j,i])
 			i++;
 			j--;
 
 		}
 		
-		animation.push([i,j])
+		if(!IS_SWITCHED){
+		animation.push([i,j])}
+		IS_SWITCHED = 0;
 
 	if (left < j)
 		quickSortR(arr, left, j);
